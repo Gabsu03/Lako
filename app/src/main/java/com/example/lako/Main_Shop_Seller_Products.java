93f37294b0bb3318b1e3d2d5ddf8ab7e97f66f6c
Elissa Mae C. Duff <8212543@ntc.edu.ph>
2024-12-27 17:10:22 +0800

    hehehe

--- app/src/main/java/com/example/lako/Main_Shop_Seller_Products.java
+++ app/src/main/java/com/example/lako/Main_Shop_Seller_Products.java
@@ -26,47 +26,14 @@ import java.util.List;
 
 public class Main_Shop_Seller_Products extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private ProductAdapter productAdapter;
-    private List<Product> productList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_shop_seller_products);
 
-        recyclerView = findViewById(R.id.products_display_recycler_vieww);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Load product data from SharedPreferences
-        loadProductData();
-
-        // Set up the adapter
-        productAdapter = new ProductAdapter(productList);
-        recyclerView.setAdapter(productAdapter);
     }
-
-    private void loadProductData() {
-        SharedPreferences sharedPreferences = getSharedPreferences("ProductData", MODE_PRIVATE);
-        String json = sharedPreferences.getString("products", "[]");
-
-        // Parse the JSON string into a list of Product objects
-        Gson gson = new Gson();
-        Type type = new TypeToken<List<Product>>() {}.getType();
-        List<Product> loadedProductList = gson.fromJson(json, type);
-
-        // Check if the loaded list is not empty
-        if (loadedProductList != null && !loadedProductList.isEmpty()) {
-            productList.addAll(loadedProductList);
-        } else {
-            Log.d("Main_Shop_Seller_Products", "No products found in SharedPreferences.");
-        }
-
-        // Notify the adapter that the data has changed
-        productAdapter.notifyDataSetChanged();
-    }
-
-
 
     public void my_shop_profile_back_btn(View view) {
         startActivity(new Intent(Main_Shop_Seller_Products.this, Profile_User.class));

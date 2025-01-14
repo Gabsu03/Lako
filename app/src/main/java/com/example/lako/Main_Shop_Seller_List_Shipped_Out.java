0a689679c397f7b4cdfc65b9e87400fa2e8866cb
Elissa Mae C. Duff <8212543@ntc.edu.ph>
2025-01-15 00:36:38 +0800

    Seller

---
+++ app/src/main/java/com/example/lako/Main_Shop_Seller_List_Shipped_Out.java
@@ -0,0 +1,19 @@
+package com.example.lako;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Main_Shop_Seller_List_Shipped_Out extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_shop_seller_list_shipped_out);
+    }
+}

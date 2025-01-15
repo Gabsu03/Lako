bb95be3e5d9fd7b5d8149faef589ec0b8235de65
Elissa Mae C. Duff <8212543@ntc.edu.ph>
2025-01-15 21:55:36 +0800

    ui

---
+++ app/src/main/java/com/example/lako/Cancel_Order_Splash_Screen.java
@@ -0,0 +1,19 @@
+package com.example.lako;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Cancel_Order_Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cancel_order_splash_screen);
+    }
+}
